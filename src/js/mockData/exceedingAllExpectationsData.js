fb13be84bda9eb52d22d24f7075702c328c3ad12
dneis2489 <denis-pelenev@inbox.ru>
2024-03-06 11:10:49 +0500

    Объявил иниты и данные

---
+++ src/js/mockData/exceedingAllExpectationsData.js
@@ -0,0 +1,16 @@
+export const illustration = {
+    src: "./assets/img/Illustration.png",
+};
+
+export const header =
+    "Превосходя все ожидания";
+
+export const description =
+    `Однако кровать для помощи в путешествии неприятна. 
+  Не мысли все осуществляют благословение. Снисхождение ко всему, радость, изменение бурной привязанности. Вечеринку мы лет на заказ разрешили.`;
+
+export const exceedingAllExpectationsData = {
+    illustration,
+    header,
+    description
+};
--- src/js/ui/pages/homePage.js
+++ src/js/ui/pages/homePage.js
@@ -1,9 +1,17 @@
+import initBurger from "./../components/initBurger.js";
+
 import initHeader from "./../components/initHeader.js";
 import initHero from "./../components/initHero.js";
 import initBrands from "./../components/initBrands.js";
 import initWhatIsGpt from "./../components/initWhatIsGpt.js";
 import initFutureHere from "./../components/initFutureHere.js";
-import initBurger from "./../components/initBurger.js";
+import initExceedingAllExpectations from "./../components/initExceedingAllExpectations.js";
+import initRegistration from "./../components/initRegistration.js";
+import initBlogSection from "./../components/initBlogSection.js";
+
+import initStepIntoFuture from "./../components/initStepIntoFuture.js";
+import initFooter from "./../components/initFooter.js";
+import initLawProtected from "./../components/initLawProtected.js";
 
 const initHomePage = (element) => {
   const template = `
@@ -12,6 +20,13 @@ const initHomePage = (element) => {
     <section class="section brands_section"></section>
     <section class="section what_is_chatgpt_section"></section>
     <section class="section future_here"></section>
+    <section class="section exceeding_all_expectations"></section>
+    <section class="section registration"></section>
+    <section class="section blog_section"></section>
+
+    <section class="section step_into_future"></section>
+    <section class="section footer"></section>
+    <section class="law_protected"></section>
   `;
 
   element.insertAdjacentHTML("beforeend", template);
@@ -35,15 +50,31 @@ const homePage = () => {
   // initHero(rootNode2);
 
   // инициализация хиро раздела
-  initBrands();
+  initBrands(rootNode1);
 
   // инициализация хиро раздела
-  initWhatIsGpt();
+  initWhatIsGpt(rootNode1);
 
   // инициализация раздела "Будущее наступило" с мок датой
-  initFutureHere();
+  initFutureHere(rootNode1);
 
+  // инициализация раздела "Превосходя все ожидания" с мок датой
+  initExceedingAllExpectations(rootNode1);
 
+  // инициализация раздела "Зарегистрируйся" с мок датой
+  initRegistration(rootNode1);
+
+  // инициализация раздела "Блог" с мок датой
+  initBlogSection(rootNode1)
+
+  // инициализация раздела "Шагнуть в будущее" с мок датой
+  initStepIntoFuture(rootNode2)
+
+  // инициализация раздела "Футер" с мок датой
+  initFooter(rootNode2)
+
+  // инициализация раздела "Все права защищены" с мок датой
+  initLawProtected(rootNode2)
 
   // инициализация бургера для адаптивного меню
   initBurger();
